f243606d349d4fb29cd74d73089b4e7cee37e226
viki1 <v.golubova@denexy.com>
2022-12-21 15:05:49 +0400

    Install vite-plugin-pug, sass, sass-loader, bootstrap, bootstrap-vue-3.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    modules: ['bootstrap-vue-3/nuxt'],
+    css: ['bootstrap/dist/css/bootstrap.css'],
 })
