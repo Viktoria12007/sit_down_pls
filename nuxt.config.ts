842f70ae5f89122ca39946822e699f7a2b1425aa
viki1 <v.golubova@denexy.com>
2023-01-19 13:34:14 +0400

    Settings preprocessor options for vite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,13 @@
 export default defineNuxtConfig({
     modules: ['bootstrap-vue-3/nuxt'],
     css: ['bootstrap/dist/css/bootstrap.css'],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                sass: {
+                    additionalData: '@use "@/assets/main.sass" as *\n'
+                }
+            }
+        }
+    }
 })
