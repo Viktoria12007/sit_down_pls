8be4c217dfad7144995cf15261b887104e157e6b
viki1 <v.golubova@denexy.com>
2023-01-19 13:39:04 +0400

    Create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+app-header
+</template>
+
+<script>
+import AppHeader from '../components/AppHeader';
+
+export default {
+  name: 'Default',
+  components: { AppHeader },
+}
+</script>
+
+<style scoped>
+</style>
